613e04d1de0b64208f95b069972a7f15198430bf
Pacifique MUYIRINGIRE <pmuyiringire@bk.rw>
2025-12-30 20:53:49 +0200

    Converted energy_readings to hypertable with different chunks

---
+++ sql/02_hypertables.sql
@@ -0,0 +1,23 @@
+-- Convert main table to hypertable with 1-day chunks
+SELECT create_hypertable(
+    'energy_readings', 
+    'timestamp', 
+    chunk_time_interval => INTERVAL '1 day',
+    if_not_exists => TRUE,
+    migrate_data => TRUE  -- If you already have data in energy_readings
+);
+
+-- Create hypertables with different chunk intervals
+SELECT create_hypertable(
+    'energy_readings_3h', 
+    'timestamp', 
+    chunk_time_interval => INTERVAL '3 hours',
+    if_not_exists => TRUE
+);
+
+SELECT create_hypertable(
+    'energy_readings_week', 
+    'timestamp', 
+    chunk_time_interval => INTERVAL '1 week',
+    if_not_exists => TRUE
+);
